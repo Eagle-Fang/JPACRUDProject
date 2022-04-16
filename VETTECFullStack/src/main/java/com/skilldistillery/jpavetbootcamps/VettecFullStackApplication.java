005825f711f4128e237c01c3f99451cea241c37b
Ving <ving@Vings-MacBook-Pro.local>
2022-04-15 21:30:46 -0400

    Start Boot MVC

---
+++ VETTECFullStack/src/main/java/com/skilldistillery/jpavetbootcamps/VettecFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpavetbootcamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VettecFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VettecFullStackApplication.class, args);
+	}
+
+}
---
+++ VETTECFullStack/src/test/java/com/skilldistillery/jpavetbootcamps/VettecFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpavetbootcamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VettecFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
